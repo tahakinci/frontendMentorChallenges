4f21b5a31b837a89804e2382846020461e315413
Muhammed Taha Akinci <106470797+tahakinci@users.noreply.github.com>
2023-09-23 23:31:00 +0300

    challenges

---
+++ interactive-rating-component-main/index.js
@@ -0,0 +1,22 @@
+let card = document.querySelector(".score-box");
+let cardBtn = document.querySelector(".button");
+let cardNums = document
+  .querySelector(".score-container")
+  .querySelectorAll("button");
+let selectNum = document.querySelector(".selected-score");
+let num = "";
+
+cardNums.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    cardNums.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+
+    num = btn.innerHTML;
+  });
+});
+cardBtn.addEventListener("click", () => {
+  card.classList.toggle("active");
+  selectNum.innerHTML = num;
+});
